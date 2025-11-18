c6a8a1ee4c0b3d5118f3ee22d3cfd6d19e072e36
Farhan-Syarif-H <parhan15905n@gmail.com>
2025-11-18 11:21:22 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:jurnalku_clone/explore_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Jurnalku Clone',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const ExplorePage(),
+    );
+  }
+}
